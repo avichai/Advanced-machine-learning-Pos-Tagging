551ea92e8fae37d80efd69b144ee58b98cd4c779
Maor <maorivg@gmail.com>
2016-12-10 17:08:23 +0200

    new syntax - memory errors

--- pos_tag/pos_tagging.py
+++ pos_tag/pos_tagging.py
@@ -10,47 +10,29 @@ def mle (x, y, xv, yv):
         t . shape = (| val ( X )| ,| val ( X )|) , and
         e . shape = (| val ( X )| ,| val ( Y )|)
     '''
-    nij = get_nij(x, xv)
-    nyi = get_nyi(x, y, xv, yv)
+    occ_mat_X = get_occ_mat(x, xv)
+    occ_mat_Y = get_occ_mat(y, yv)
+    nij = get_nij(occ_mat_X)
+    nyi = get_nyi(occ_mat_X, occ_mat_Y)
     t_hat = get_estimator(nij)
     e_hat = get_estimator(nyi)
     return t_hat, e_hat
 
+def flatten_list(lst):
+    return np.asarray([item for sublist in lst for item in ['']+sublist+['']])
 
-def get_nij(X, S):
-    from time import time
-    t= time()
-    # new syntax
-    flattened_X = np.asarray([item for sublist in X for item in ['']+sublist+['']])
-    occ_mat = np.zeros((len(S), len(flattened_X)))
-    for i, s in enumerate(S):
-        occ_mat[i, :] = flattened_X == s
-    new_nij = np.matmul(occ_mat[:,:-1], np.transpose(occ_mat[:,1:]))
-    print ('Done new syntax in {0} seconds'.format(time() -t))
-    t= time()
+def get_occ_mat(lst, vals):
+    flat_list = flatten_list(lst)
+    occ_mat = np.zeros((len(vals), len(flat_list)))
+    for i, v in enumerate(vals):
+        occ_mat[i, :] = flat_list == v
+    return occ_mat
 
-    nij = np.zeros([len(S)]*2)
-    for seq in X:
-        seq = np.asarray(seq)
-        for i, si in enumerate(S):
-            for j, sj in enumerate(S):
-                nij[i, j] += np.sum(np.logical_and((seq == sj)[1:], (seq == si)[:-1]))
-    print('Done old syntax in {0} seconds'.format(time() - t))
-    if not np.all(new_nij == nij):
-        raise Exception("They are not the same!")
-    else:
-        print("YAY")
-        exit(0)
-    return nij
+def get_nij(occ_mat_X):
+    return np.matmul(occ_mat_X[:,:-1], np.transpose(occ_mat_X[:,1:]))
 
-def get_nyi(X, Y, S, T):
-    nyi = np.zeros(len(T), len(S))
-    for i, s in enumerate(S):
-        for j, t in enumerate(T):
-            for seqX, seqY in zip(X, Y):
-                nyi[i, j] = np.sum(np.logical_and(np.asarray(seqX) == s, np.asarray(seqY) == t))
-
-    return nyi
+def get_nyi(occ_mat_X, occ_mat_Y):
+    return np.matmul(occ_mat_Y, occ_mat_X.transpose())
 
 def get_estimator(n_mat):
     return np.divide(n_mat, np.matlib.repmat(np.sum(n_mat, 0), 1, n_mat.shape[1]))
